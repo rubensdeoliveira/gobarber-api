d32cdf377dc3afabdca31e79d0d1e79de9c95d7a
rubensojunior <rubensojunior6@gmail.com>
2020-04-24 00:14:52 -0300

    implementing jwt token

---
+++ .env.js
@@ -0,0 +1,1 @@
+export const app = { sign: '870fee1ca6c36660677957ded75dc663' }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,42 @@
+import { getRepository } from 'typeorm'
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+import User from '../models/User'
+
 interface Request {
   email: string
   password: string
 }
 
+interface Response {
+  user: User
+  token: string
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<void> {}
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User)
+
+    const user = await usersRepository.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error('E-mail ou senha inválidos')
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new Error('E-mail ou senha inválidos')
+    }
+
+    const token = sign({}, '870fee1ca6c36660677957ded75dc663', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    return { user, token }
+  }
 }
 
 export default AuthenticateUserService
