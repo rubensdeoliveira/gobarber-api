b61b58162057f8af76ebe3b9b5ca125a8356da28
rubensojunior <rubensojunior6@gmail.com>
2020-04-23 21:34:39 -0300

    implementing appointments repository method findbydate

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,10 +1,16 @@
+import { EntityRepository, Repository } from 'typeorm'
+
 import Appointment from '../models/Appointment'
 
-interface CreateAppointmentDTO {
-  provider: string
-  date: Date
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    })
 
-class AppointmentsRepository {}
+    return findAppointment || null
+  }
+}
 
 export default AppointmentsRepository
