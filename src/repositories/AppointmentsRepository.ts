88180c5be32ecde67872e2b9b5de631a5188784b
rubensojunior <rubensojunior6@gmail.com>
2020-04-23 21:27:39 -0300

    creating appointments repository

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,10 @@
+import Appointment from '../models/Appointment'
+
+interface CreateAppointmentDTO {
+  provider: string
+  date: Date
+}
+
+class AppointmentsRepository {}
+
+export default AppointmentsRepository
