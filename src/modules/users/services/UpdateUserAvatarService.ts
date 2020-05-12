394d00163723d0c09515060c56d2732639637657
rubensojunior <rubensojunior6@gmail.com>
2020-05-12 20:10:37 -0300

    adding update user avatar tests

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,7 +4,10 @@ import uploadConfig from '@config/upload'
 import { injectable, inject } from 'tsyringe'
 
 import AppError from '@shared/errors/AppError'
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 import IUsersRepository from '../repositories/IUsersRepository'
+
 import User from '../infra/typeorm/entities/User'
 
 interface IRequest {
@@ -17,6 +20,9 @@ class UpdateAvatarUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -30,15 +36,12 @@ class UpdateAvatarUserService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar)
     }
 
-    user.avatar = avatarFilename
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+
+    user.avatar = filename
 
     await this.usersRepository.save(user)
 
