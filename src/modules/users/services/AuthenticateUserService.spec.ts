5439fb901bf66525159ccd97ba927c92898b22b6
rubensojunior <rubensojunior6@gmail.com>
2020-05-12 19:29:51 -0300

    adding authenticate user service tests

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,82 @@
+import AppError from '@shared/errors/AppError'
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+import AuthenticateUserService from './AuthenticateUserService'
+import CreateUserService from './CreateUserService'
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
+
+    const user = await createUser.execute({
+      name: 'Anderson Silva',
+      email: 'anderson@spider.com.br',
+      password: '12345678',
+    })
+
+    const response = await authenticateUser.execute({
+      email: 'anderson@spider.com.br',
+      password: '12345678',
+    })
+
+    expect(response).toHaveProperty('token')
+    expect(response.user).toEqual(user)
+  })
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
+
+    expect(
+      authenticateUser.execute({
+        email: 'anderson@spider.com.br',
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
+
+    await createUser.execute({
+      name: 'Anderson Silva',
+      email: 'anderson@spider.com.br',
+      password: '12345678',
+    })
+
+    expect(
+      authenticateUser.execute({
+        email: 'anderson@spider.com.br',
+        password: 'wrong-pass',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
